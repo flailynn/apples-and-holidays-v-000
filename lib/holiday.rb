af4d73fd62f1f51de1f6164346c69f5fe3bf2b4c
Anita Beauchamp <amadamspr@gmail.com>
2018-07-27 05:27:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
     return_array = hash_with_holidays.select do |holiday, items_array|
 
       if items_array.include?("BBQ")
-        return_array << holiday
+        return_array << holiday.to_s
       end
     end
 
